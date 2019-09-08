9bc8afa17a1aa365e53b3ceb38dd6cb9a51aae85
zcong1993 <zhangcong1992@gmail.com>
2019-09-08 12:49:03 +0800

    fix btso search

--- app/src/main/java/io/github/javiewer/network/BTSO.java
+++ app/src/main/java/io/github/javiewer/network/BTSO.java
@@ -6,7 +6,7 @@ import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
-import retrofit2.http.Query;
+import retrofit2.http.Path;
 import retrofit2.http.Url;
 
 /**
